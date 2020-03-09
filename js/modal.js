7e6d70e1437ea10d8c15282f37c30e35f80f6b52
Raphael Henrique Dos Nascimento De Lima <raphael.lima@rsinet.com.br>
2020-03-09 20:02:48 -0300

    adiocionando a modal

---
+++ js/modal.js
@@ -0,0 +1,20 @@
+
+
+function iniciaModal(modalId) {
+    const modal = document.getElementById(modalId);
+    if (modal) {
+        modal.classList.add('mostrar');
+        modal.addEventListener('click', (evento) => {
+            if (evento.target.id == modalId || evento.target.className == 'fechar') {
+                modal.classList.remove('mostrar');
+            }
+        });
+    }
+
+}
+
+const addBolsa = document.querySelector('.adiocionaBolsa');
+addBolsa.addEventListener('click', () => iniciaModal('modal-bolsa'))
+
+
+
