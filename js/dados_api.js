5aa68eba77e79230d92aeb9498d11d91749c2178
Raphael Henrique Dos Nascimento De Lima <raphael.lima@rsinet.com.br>
2020-03-13 11:04:48 -0300

    consumir dados com fetch

--- js/dados_api.js
+++ js/dados_api.js
@@ -1,30 +1,5 @@
-var data = "";
-  
-  function response(teste){
-    data = teste;
-  }
- 
+fetch("./js/db.json", { method: "get" })
+        .then(response => response.json())
+        .then(data => { return Object.keys(res = data.message).map(k => k) }
 
-  function lerArquivo(nome, callback)
-  {
-      var req = new XMLHttpRequest();
-      req.open("GET", nome, false);
-      req.onreadystatechange = function ()
-      {
-          if(req.readyState === 4)
-          {
-              //verifica se a requisição foi bem sucedida
-              if(req.status === 200 || req.status == 0)
-              {
-                  callback(req.responseText);
-              }
-          }
-      }
-      req.send(null);
-  }
-
-  lerArquivo("./js/db.json",response);
-  var objects = JSON.parse(JSON.stringify(data));
-  
-  
-console.log(objects);
+        )
