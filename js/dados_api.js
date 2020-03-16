0af3068ce5256f1bb526bd464e4df0d39b4a33ec
Raphael Henrique Dos Nascimento De Lima <raphael.lima@rsinet.com.br>
2020-03-16 20:52:06 -0300

    adicionado db.json

--- js/dados_api.js
+++ js/dados_api.js
@@ -1,4 +1,4 @@
 var listaBolsas = () => {
-    return fetch('https://api-teste-quero-bolsa.herokuapp.com/', { method: "GET", mode: "cors" })
+    return fetch("./dados/db.json", { method: "GET", mode: "cors" })
         .then(response => response.json())
 }
