d43d483c823ee849ff4f96519169389c1ff5b737
Raphael Henrique Dos Nascimento De Lima <raphael.lima@rsinet.com.br>
2020-03-16 20:54:01 -0300

    adicionado db.json

--- js/dados_api.js
+++ js/dados_api.js
@@ -1,4 +1,4 @@
 var listaBolsas = () => {
-    return fetch("./dados/db.json", { method: "GET", mode: "cors" })
+    return fetch("./js/dados/db.json", { method: "GET", mode: "cors" })
         .then(response => response.json())
 }
